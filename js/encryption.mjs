d0d30e54d968356692d323973bc08d47a635fc46
ddsol2 <note627462@gmail.com>
2025-09-15 13:28:59 +0000

    Seems to be wrx now

--- js/encryption.mjs
+++ js/encryption.mjs
@@ -1,33 +1,12 @@
-// The goal is a generator that creates a function that decodes a string.
-// The string is a secret that should not be in the source code.
-// The generator will be run manually to create the function.
-// The function will be called at runtime to get the secret.
-// The secret is in the form of a string.
-
-// The API is that we take in a string and return an encoder/decoder pair
-
-
-
-/*
-Get some key material to use as input to the deriveKey method.
-The key material is a password supplied by the user.
-*/
-
-async function encrypt(password, plaintext, salt, iv) {
-  const key = await getPasswordKey(password);
-
-  return window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, plaintext);
-}
-
 const keys = {};
 
 export default function makeEncryptor(password) {
-
   async function getPasswordKey(password) {
     if (keys[password]) return keys[password];
+    const salt = new Uint8Array([21, 31, 41, 51, 61, 71, 81, 91]);
     return keys[password] = await (async () => {
       const enc = new TextEncoder();
-      const raw = window.crypto.subtle.importKey(
+      const raw = await window.crypto.subtle.importKey(
         "raw",
         enc.encode(password),
         "PBKDF2",
@@ -41,7 +20,7 @@ export default function makeEncryptor(password) {
           iterations: 100000,
           hash: "SHA-256",
         },
-        keyMaterial,
+        raw,
         { name: "AES-GCM", length: 256 },
         true,
         ["encrypt", "decrypt"],
@@ -51,7 +30,7 @@ export default function makeEncryptor(password) {
       return key;
     })();
   }
-  async function encrypt(message, initializationVector) {
+  async function encrypt(message, initializationVector = new ArrayBuffer(16)) {
     const key = await getPasswordKey(password);
     const encoder = new TextEncoder();
     encodedMessage = encoder.encode(message);
@@ -63,7 +42,7 @@ export default function makeEncryptor(password) {
     );
   }
 
-  async function decrypt(ciphertext, initializationVector) {
+  async function decrypt(ciphertext, initializationVector = new ArrayBuffer(16)) {
     const key = await getPasswordKey(password);
     const decryptedText = await window.crypto.subtle.decrypt(
       // The iv value must be the same as that used for encryption
