5c7d9dca17302df769d4b584e4ee78e98937fa59
Han de Boer <note627462@gmail.com>
2026-03-13 14:22:40 +0100

    Moar

--- src/boot-test/src/main.ts
+++ src/boot-test/src/main.ts
@@ -6,3 +6,4 @@ document.body.innerHTML += `<h1>${greeting}</h1>`;
 // Test a breaking change logic simulation
 const result = 5 + 5; 
 document.body.innerHTML += `<p>Calculation Test: ${result}</p>`;
+throw new Error("Simulated error to test breaking source maps.");
